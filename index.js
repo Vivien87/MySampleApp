37281c2998be1a2465121e0455a4f0ef36500b84
Volodymyr Dibrova <VolodymyrDibrova@dmytroslotvinskyygmail.onmicrosoft.com>
2025-02-18 19:53:07 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello from MySampleApp!");
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>My Stateless App</title>
+        <style>
+            body { background-color: yellow; }
+        </style>
+    </head>
+    <body>
+        <h1>Welcome to Stateless App v2</h1>
+    </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
